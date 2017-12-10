95ba40d8cbecf3dccc4d096fc9fa0f7cfbf9b0d7
wcq881215@163.com <nm19881215>
2017-12-10 14:00:41 +0800

    lose

---
+++ spring-cloud-web/src/main/java/com/csf/web/repository/OrderDeviceDao.java
@@ -0,0 +1,10 @@
+package com.csf.web.repository;
+
+import com.csf.web.entity.OrderDevice;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by changqi.wu on 17-11-10.
+ */
+public interface OrderDeviceDao extends CrudRepository<OrderDevice, Long> {
+}
